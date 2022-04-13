830b46f57fd87405511f579304232e42f85bd878
혁쨩 <lim0202jh@naver.com>
2022-04-14 00:06:04 +0900

    4.14 login

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import LoginForm from './login/login';
 import Main from './Main/main';
 
 function App() {
-  return <Main />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/college-mate/login" element={<LoginForm />}></Route>
+        <Route path="/college-mate/main" element={<Main />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/login/login_right.js
+++ src/login/login_right.js
@@ -1,27 +1,79 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './login.css';
 import BadgeIcon from '@mui/icons-material/Badge';
 import LockIcon from '@mui/icons-material/Lock';
+import axios from 'axios';
 
 function Login_r() {
+  const [inputId, setInputId] = useState('');
+  const [inputPw, setInputPw] = useState('');
+
+  // input data의 변화가 있을 때마다 value에 값을 넣어줌.
+  const handleInputId = (e) => {
+    setInputId(e.target.value);
+  };
+
+  const handleInputPw = (e) => {
+    setInputPw(e.target.value);
+  };
+
+  const onKeyPress = (e) => {
+    if (e.key == 'Enter') {
+      onClickLogin();
+    }
+  };
+
+  // login 버튼 클릭 이벤트
+  const onClickLogin = () => {
+    console.log('click');
+    axios
+      .post('/login', null, {
+        params: {
+          username: inputId,
+          password: inputPw,
+        },
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
   return (
     <div className="loginForm">
       <h2>로그인</h2>
       <div className="l_content">
-        <form>
+        <div className="Form">
           <div>
             <BadgeIcon className="icon" />
-            <input type="text" name="id" className="box" placeholder="아이디를 입력하세요" />
+            <input
+              type="text"
+              name="id"
+              value={inputId}
+              onChange={handleInputId}
+              className="box"
+              placeholder="아이디를 입력하세요"
+            />
           </div>
           <div>
             <LockIcon className="icon" />
-            <input type="password" name="pw" className="box" placeholder="비밀번호를 입력하세요" />
+            <input
+              type="password"
+              name="pw"
+              value={inputPw}
+              className="box"
+              onChange={handleInputPw}
+              onKeyPress={onKeyPress}
+              placeholder="비밀번호를 입력하세요"
+            />
           </div>
           <a href="#">Forget Password?</a>
-          <button className="login_btn">
-            <input type="submit" value="Login" />
+          <button className="login_btn" onClick={onClickLogin}>
+            로그인
           </button>
-        </form>
+        </div>
         <span>
           <p>New here? </p>
           <a href="#">Create on Account</a>
