5faf930c274b4cb11e7705c808e77b50884919cb
혁쨩 <lim0202jh@naver.com>
2022-04-14 18:13:45 +0900

    11

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import LoginForm from './login/login';
 import Main from './Main/main';
-import Signup from './login/Signup';
 
 function App() {
   return (
@@ -10,7 +9,6 @@ function App() {
       <Routes>
         <Route path="/college-mate/login" element={<LoginForm />}></Route>
         <Route path="/college-mate/main" element={<Main />}></Route>
-        <Route path="/college-mate/Sign" element={<Signup />}></Route>
       </Routes>
     </BrowserRouter>
   );
